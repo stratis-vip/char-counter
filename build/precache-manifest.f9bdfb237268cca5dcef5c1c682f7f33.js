f269cf5e40d740720df929b028a596ac7bcbecdb
stratis-vip <stratis.vip@gmail.com>
2020-07-20 00:24:39 +0300

    build

---
+++ build/precache-manifest.f9bdfb237268cca5dcef5c1c682f7f33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23fe7207eb4b29a717682adcbd3e7f49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e08ff38dbfb7aa534af",
+    "url": "/static/css/main.794bd635.chunk.css"
+  },
+  {
+    "revision": "ab45287e46418908dde4",
+    "url": "/static/js/2.a7ff7a39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a7ff7a39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e08ff38dbfb7aa534af",
+    "url": "/static/js/main.71c9506c.chunk.js"
+  },
+  {
+    "revision": "a09792300aedfe1060d4",
+    "url": "/static/js/runtime-main.d8921639.js"
+  }
+]);
